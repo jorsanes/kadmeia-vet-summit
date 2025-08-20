aa6a77cb43d91b344a58b65fc5f7074c8d33f642
jorsanes <jorsanes@hotmail.com>
2025-08-20 12:08:59 +0200

    Create HrefLAngLinks.tsx

---
+++ src/components/layout/HrefLAngLinks.tsx
@@ -0,0 +1,6 @@
+export default function HrefLangLinks() {
+  if (import.meta.env.DEV) {
+    console.warn("[HrefLangLinks] deprecated: usa <PageSeo/>. Este componente no hace nada.");
+  }
+  return null;
+}
