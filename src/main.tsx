9a63b98010774aeef9e409b10435c1692fa7bea4
jorsanes <jorsanes@hotmail.com>
2025-08-20 11:03:09 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,11 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-
-createRoot(document.getElementById("root")!).render(<App />);
+import { HelmetProvider } from "react-helmet-async";
+import SeoBase from "@/components/seo/SeoBase";
+// ...
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <HelmetProvider>
+      <SeoBase />
+      {/* Tu Router/App existente */}
+    </HelmetProvider>
+  </React.StrictMode>
+);
